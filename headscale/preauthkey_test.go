5022303092a75dd7b54cacd7ee7917e478c84e8d
adrien margueron <adrien.margueron@navya.tech>
2023-03-09 11:51:21 +0100

    feat(headsacle:pak) add preauthkey management functions + unit tests

---
+++ headscale/preauthkey_test.go
@@ -0,0 +1,64 @@
+package headscale
+
+import (
+	"context"
+	"time"
+	// "fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreatePreAuthKey(t *testing.T){
+	
+	c := NewClient()
+	c.ApiURL = "http://localhost:8080"
+	c.ApiKey = "sXqH2YEY7Q.Lj-d4ywYsGWLhCiYk9oFIYM-ZrSrq07uxBMpaTbrB_s"
+	existingUserName := "bar"
+	nonExistingUserName := "baz"
+	c.CreateUser(context.Background(), existingUserName)
+	c.DeleteUser(context.Background(), nonExistingUserName)
+	testCases := []struct {
+		pakConfig 						PreAuthKeyConfig
+		name 									string
+		wantError							error
+		wantPreAuthKeyStatus 	PreAuthKeuStatus
+	}{
+			{
+				name: "Simplest request",
+				pakConfig: PreAuthKeyConfig{
+					User: existingUserName,
+				},
+				wantError: nil,
+				wantPreAuthKeyStatus: preAuthKeyCreated,
+			},
+			{
+				name: "User does not exists",
+				pakConfig: PreAuthKeyConfig{
+					User: nonExistingUserName,
+				},
+				wantError: ErrUserNotFound,
+				wantPreAuthKeyStatus: preAuthKeyError,
+			},
+			{
+				name: "Rrquest With all parameters",
+				pakConfig: PreAuthKeyConfig{
+					User: existingUserName,
+					Reusable: true,
+					Ephemeral: false,
+					Expiration: time.Now().Add(time.Hour),
+					Tags: []string{"hello","world"},
+				},
+				wantError: nil,
+				wantPreAuthKeyStatus: preAuthKeyCreated,
+			},
+	}
+	for _, tc := range testCases {
+		tc := tc 
+		t.Run(tc.name, func(t *testing.T){
+			preAuthKeyStatus, _, err := c.CreatePreAuthKey(context.Background(),tc.pakConfig)
+			assert.ErrorIs(t,err,tc.wantError)
+			assert.Equal(t, tc.wantPreAuthKeyStatus,preAuthKeyStatus)
+		})
+	}
+}
